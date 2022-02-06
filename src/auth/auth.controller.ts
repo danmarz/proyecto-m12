d55226163fd8f5138cbad019d8a79fe93412b99d
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2022-02-06 01:36:15 +0100

    login route with AuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Request() request) {
+    return {
+      email: request.user.email,
+      token: 'the token will go here',
+    };
+  }
+}
