c9fb46c9e53023f63e1fb1b0f7dd9c90b2ebf23d
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2022-02-06 02:08:17 +0100

    JWT config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (ConfigService: ConfigService) => ({
+        secret: ConfigService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '60m',
+        },
+      }),
+    }),
+  ],
   providers: [LocalStrategy],
   controllers: [AuthController],
 })
